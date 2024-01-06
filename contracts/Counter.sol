e8b407d24f554791116c6c59c48aa46be80fc928
Vanshika-Rana <ranavanshika172000@gmail.com>
2024-01-06 12:16:10 +0530

    Counter contract added

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,2 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract Counter{
+    uint256 private _counter;
+
+    event CounterIncreased(uint256 Count);
+
+    function increaseCounter() external {
+        _counter += 1;
+
+    }
+    function getCounter() external view returns (uint256){
+        return _counter;
+    }
+
+}
